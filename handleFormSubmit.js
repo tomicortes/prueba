7fa42223cce7ea9da4bb2e122d0145ad422495c5
Tomás Cortés <107221704+tomicortes@users.noreply.github.com>
2022-11-03 15:26:18 -0300

    Add files via upload

--- handleFormSubmit.js
+++ handleFormSubmit.js
@@ -1,12 +1,11 @@
 function handleFormSubmit(event) {
   event.preventDefault()
   const inputNombre = document.getElementById("product-name")
-  const product = new Product (inputNombre.value)
+  
   const inputDescripcion = document.getElementById("product-description")
-  const descripcion = new Product (inputDescripcion.value)
+ 
   const inputPrecio = document.getElementById("product-price")
-  const precio = new Product (inputPrecio.value)
+  const product = new Product(inputNombre.value,inputDescripcion.value,inputPrecio.value)
+
   addProduct(product,container)
-  addProduct(descripcion,container)
-  addProduct(precio,container)
 }
